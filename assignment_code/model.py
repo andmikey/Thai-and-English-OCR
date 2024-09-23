af703382324bf3ca76637f022f00ec4bb1f19fc0
Mikey <31766607+andmikey@users.noreply.github.com>
2024-09-23 09:05:39 +0200

    Implement model

--- assignment_code/model.py
+++ assignment_code/model.py
@@ -1,14 +1,25 @@
 import torch.nn as nn
 
 
-class BasicNetwork(nn.module):
-    def __init__(self):
-        super(BasicNetwork, self).__init__(self)
-        # TODO some definitions here
+class BasicNetwork(nn.Module):
+    def __init__(self, num_classes, img_dims):
+        # Some borrowing from this, adjusted to the right sizes for a 64x64 image:
+        # https://medium.com/@deepeshdeepakdd2/lenet-5-implementation-on-mnist-in-pytorch-c6f2ee306e37
+        # TODO don't hard-code image size
+        super().__init__()
+        conv1 = nn.Conv2d(in_channels=1, out_channels=6, kernel_size=5, padding=2)
+        avgp = nn.AvgPool2d(kernel_size=4, stride=2)
+        conv2 = nn.Conv2d(in_channels=6, out_channels=16, kernel_size=5)
+        fl = nn.Flatten()
+        fc = nn.Linear(in_features=12 * 12 * 16, out_features=120)
+        tanh = nn.Tanh()
+        fc2 = nn.Linear(in_features=120, out_features=84)
+        fc3 = nn.Linear(in_features=84, out_features=num_classes)
 
-    def forward(self, input):
-        # TODO return something here
-        # Example net here: https://pytorch.org/tutorials/beginner/blitz/neural_networks_tutorial.html
-        # And here: https://pytorch.org/tutorials/beginner/blitz/cifar10_tutorial.html
-        output = _
-        return output
+        self.feature = nn.Sequential(conv1, tanh, avgp, conv2, tanh, avgp)
+        self.classifier = nn.Sequential(fl, fc, tanh, fc2, tanh, fc3)
+
+    def forward(self, x):
+        # Needs to return tensor in the shape (num_classes)
+        # Criterion CrossEntropyLoss takes class index
+        return self.classifier(self.feature(x))
