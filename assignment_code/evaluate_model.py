dd8bc92941b70fb1f83cd1fb16a7cec6bcfb09d5
Mikey <31766607+andmikey@users.noreply.github.com>
2024-09-23 09:08:21 +0200

    Update evaluation code

--- assignment_code/evaluate_model.py
+++ assignment_code/evaluate_model.py
@@ -1,3 +1,4 @@
+import logging
 from pathlib import Path
 
 import click
@@ -7,16 +8,15 @@ from model import BasicNetwork
 
 
 @click.command()
-@click.option("--data", type=click.Path(exists=True, path_type=Path))
+@click.option("--test-data", type=click.Path(exists=True, path_type=Path))
 @click.option("--model-path", type=click.Path(exists=True, path_type=Path))
 @click.option("--batches", type=int, default=1)
 @click.option("--save-dir", type=click.Path(exists=True, path_type=Path))
-def main(data, model_path, batches, save_dir):
-    _, test, _ = utils.load_datasets(data, batches)
-    model = BasicNetwork()
+def main(test_data, model_path, batches, save_dir):
+    test = utils.load_datasets(test_data, batches)
+    model = BasicNetwork(utils.NUM_CLASSES, 64)
     model.load_state_dict(torch.load(model_path / "model.pth", weights_only=True))
 
-    # TODO may need to do some transforms here if different dpi, use resize: https://pytorch.org/vision/main/generated/torchvision.transforms.Resize.html
     pred_classes_test = model(test.dataset.x)
     test_eval = utils.Evaluator(pred_classes_test, test.dataset.y)
-    print(f"Test evaluation: {test_eval}")
+    logging.info(f"Test evaluation: {test_eval}")
