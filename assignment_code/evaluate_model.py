c9fa5a8e4bcea673398281b8205f41d850ab45d0
Mikey <31766607+andmikey@users.noreply.github.com>
2024-09-23 22:10:43 +0200

    Fix evaluation

--- assignment_code/evaluate_model.py
+++ assignment_code/evaluate_model.py
@@ -8,15 +8,36 @@ from model import BasicNetwork
 
 
 @click.command()
-@click.option("--test-data", type=click.Path(exists=True, path_type=Path))
+@click.option(
+    "--test-data",
+    type=click.Path(exists=True, path_type=Path),
+    multiple=True,
+    required=True,
+)
 @click.option("--model-path", type=click.Path(exists=True, path_type=Path))
 @click.option("--batches", type=int, default=1)
 @click.option("--save-dir", type=click.Path(exists=True, path_type=Path))
 def main(test_data, model_path, batches, save_dir):
-    test = utils.load_datasets(test_data, batches)
-    model = BasicNetwork(utils.NUM_CLASSES, 64)
-    model.load_state_dict(torch.load(model_path / "model.pth", weights_only=True))
+    # Set up logging
+    logging.basicConfig(
+        filename=save_dir / "training.log",
+        filemode="a",
+        level=logging.INFO,
+        format="%(asctime)s - %(levelname)s - %(message)s",
+    )
+    logger = logging.getLogger(__name__)
 
-    pred_classes_test = model(test.dataset.x)
-    test_eval = utils.Evaluator(pred_classes_test, test.dataset.y)
-    logging.info(f"Test evaluation: {test_eval}")
+    model = BasicNetwork(utils.NUM_CLASSES, 64)
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    model.load_state_dict(torch.load(model_path / "model.pth", weights_only=True))
+    model.to(device)
+
+    test = utils.load_datasets(test_data, batches)
+    test_eval = utils.Evaluator(model, test, device)
+    test_eval.run_on_input()
+    test_eval.calculate_metrics()
+    logger.info(f"Test evaluation: {test_eval}")
+
+
+if __name__ == "__main__":
+    main()
