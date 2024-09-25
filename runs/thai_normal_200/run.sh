e2e8614c0cc8832f7dd4603676f43a5d7c865078
Mikey <31766607+andmikey@users.noreply.github.com>
2024-09-25 20:09:21 +0200

    Simplify run names

--- runs/thai_normal_200/run.sh
+++ runs/thai_normal_200/run.sh
@@ -5,7 +5,7 @@ experiment_path=$(pwd)
 # Set up directories
 mkdir -p $experiment_path/data/ $experiment_path/outputs/
 
-# Generate training data
+# Generate training data with default ratio of train/test/val (0.6/0.2/0.2)
 python3 ../../assignment_code/generate_training_data.py \
     -l Thai -d 200 -s normal \
     --output_path $experiment_path/data/
@@ -15,7 +15,7 @@ python3 ../../assignment_code/train_model.py \
     --train-data $experiment_path/data/training_set.txt \
     --validation-data $experiment_path/data/validation_set.txt \
     --save-dir $experiment_path/outputs/ \
-    --batches 10 --epochs 2
+    --batches $NUM_BATCHES --epochs $NUM_EPOCHS
 
 # Evaluate the model
 python3 ../../assignment_code/evaluate_model.py \
