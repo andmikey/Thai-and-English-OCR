599d7487e862bb1fc1b3b749ed7be2aa07c4c37e
Mikey <31766607+andmikey@users.noreply.github.com>
2024-09-25 20:35:57 +0200

    Make all the bash scripts executable

--- runs/do_all_runs.sh
+++ runs/do_all_runs.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 set -eo pipefail
 
-NUM_BATCHES=20
-NUM_EPOCHS=50
+NUM_BATCHES=10
+NUM_EPOCHS=2
 export NUM_BATCHES
 export NUM_EPOCHS
 
 # Activate conda env
-conda activate /scratch/gusandmich/assignment_1_scratch
+# conda activate /scratch/gusandmich/assignment_1_scratch
 
-run_names=('all_thai_all_thai' 'all_thai_english_jointly'
+run_names=('all_thai' 'all_thai_english_jointly'
     'thai_bold_thai_normal' 'thai_english_normal_jointly'
     'thai_normal_200' 'thai_normal_400_thai_bold_400'
     'thai_normal_400_thai_normal_200')
@@ -17,5 +17,7 @@ run_names=('all_thai_all_thai' 'all_thai_english_jointly'
 # run_names=('thai_normal_200') # For testing things work
 
 for run_name in "${run_names[@]}"; do
-    bash $run_name/run.sh
+    ./$run_name/run.sh
 done
+
+conda deactivate
