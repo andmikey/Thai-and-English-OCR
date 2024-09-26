b6cd536ddd3d9d2b0a2bfef9488c615b3d2d4578
Mikey <31766607+andmikey@users.noreply.github.com>
2024-09-26 08:40:19 +0200

    Add a softmax at the end of the model output

--- assignment_code/model.py
+++ assignment_code/model.py
@@ -5,7 +5,6 @@ class BasicNetwork(nn.Module):
     def __init__(self, num_classes, img_dims):
         # Some borrowing from this, adjusted to the right sizes for a 64x64 image:
         # https://medium.com/@deepeshdeepakdd2/lenet-5-implementation-on-mnist-in-pytorch-c6f2ee306e37
-        # TODO don't hard-code image size
         super().__init__()
         conv1 = nn.Conv2d(in_channels=1, out_channels=6, kernel_size=5, padding=2)
         avgp = nn.AvgPool2d(kernel_size=4, stride=2)
@@ -15,9 +14,11 @@ class BasicNetwork(nn.Module):
         tanh = nn.Tanh()
         fc2 = nn.Linear(in_features=120, out_features=84)
         fc3 = nn.Linear(in_features=84, out_features=num_classes)
+        # Adjust output so all probabilities sum to 1
+        softmax = nn.Softmax()
 
         self.feature = nn.Sequential(conv1, tanh, avgp, conv2, tanh, avgp)
-        self.classifier = nn.Sequential(fl, fc, tanh, fc2, tanh, fc3)
+        self.classifier = nn.Sequential(fl, fc, tanh, fc2, tanh, fc3, softmax)
 
     def forward(self, x):
         # Needs to return tensor in the shape (num_classes)
