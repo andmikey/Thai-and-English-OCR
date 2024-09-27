ec79c538fee54bda4e10f886b9b523baf6a4d8f0
Mikey <31766607+andmikey@users.noreply.github.com>
2024-09-27 08:01:36 +0200

    Delete example scripts

--- example_scripts/train_model.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-set -eo pipefail
-
-python3 ../assignment_code/train_model.py --data ~/assignment_1/runs/test_run/data --batches 64 --epochs 100 --save_dir ~/assignment_1/runs/test_run/output
